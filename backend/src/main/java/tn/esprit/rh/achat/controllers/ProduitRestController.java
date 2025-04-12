7c131dfe5952346a5c37faaa8d4843d9e0e0545b
douabaghdadi <azizbelhadj51@gmail.com>
2025-04-13 00:30:46 +0300

    Mise à jour du fichier .gitignore

--- backend/src/main/java/tn/esprit/rh/achat/controllers/ProduitRestController.java
+++ backend/src/main/java/tn/esprit/rh/achat/controllers/ProduitRestController.java
@@ -34,7 +34,7 @@ public class ProduitRestController {
 	}
 
 	/* Ajouter en produit tout en lui affectant la catégorie produit et le stock associés */
-	// http://localhost:8089/SpringMVC/produit/add-produit/{idCategorieProduit}/{idStock}
+	// http://localhost:8089/SpringMVC/produit/add-produit
 	@PostMapping("/add-produit")
 	@ResponseBody
 	public Produit addProduit(@RequestBody Produit p) {
